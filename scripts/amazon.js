708120853182c74a0c640df20db8e1cb4dbc30b9
SohaylaGH <ghandorsouhaila@gmail.com>
2024-08-31 02:04:48 +0200

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg' ,
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs' ,
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090,
-
-
-},{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg' ,
-    name: 'Intermediate Size BasketBall' ,
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095,
-
-
-},{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg' ,
-    name: 'Adults Plain Cotton T-Shirt -2 Pack' ,
-    rating: {
-        stars: 4,
-        count: 56
-    },
-    priceCents: 799,
-
-
-}];
-
 let productHTML =`` ;
 
 products.forEach((product)=>{
